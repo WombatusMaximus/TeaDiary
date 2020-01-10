a6b4acf5c3350bfac8403cf1ba9059eecc7c995e
Wombatus <dovalkuvit@gmail.com>
2020-01-10 16:30:32 +0300

    Separated TeaListDisplayer constnants from the rest

--- TeaDiary/Scripts/App/constants.js
+++ TeaDiary/Scripts/App/constants.js
@@ -8,20 +8,3 @@ const SEARCT_TYPE = {
     NAME: "name",
     TYPE: "type" 
 };
-
-const TEA_LIST_TITLE = "Your tea list:";
-const NO_TEAS_TITLE = "No teas were found :(";
-
-const TEA_ELEMENT = {
-    PREFIX: "displayedTeaElement",
-    ID: "Id",
-    CHECKBOX: "Checkbox",
-    LINK: "Link"
-}
-
-const MOAR = "More...";
-
-const NUMBER = "#";
-const NAME = "Name";
-const TYPE = "Type";
-const LINK = "Details";
