dd151393941f75b63a46fa99422101dc6c51f1b4
Wombatus <dovalkuvit@gmail.com>
2020-01-10 18:51:07 +0300

    Added filter to main page

--- TeaDiary/Scripts/App/Home/Index.js
+++ TeaDiary/Scripts/App/Home/Index.js
@@ -1,6 +1,33 @@
 ﻿$(document).ready(documentLoaded);
 
+var displayer;
+
 function documentLoaded() {
-    var displayer = new TeaListDisplayer("#TeaList");
+    displayer = new TeaListDisplayer("#TeaList");
+    
+    $("#SearchType")
+        .append($("<option>")
+            .val(SEARCT_TYPE.NAME)
+            .html("By " + SEARCT_TYPE.NAME));
+    $("#SearchType")
+        .append($("<option>")
+            .val(SEARCT_TYPE.TYPE)
+            .html("By " + SEARCT_TYPE.TYPE));
+
     displayer.loadAndDisplay();
+}
+
+function onSearchClick() {
+    if ($("#SearchRequest").val() === "") {
+        displayer.loadAndDisplay();
+    } else {
+        var search = $("#SearchType").val() === SEARCT_TYPE.NAME ? apiQueries.searchByName : apiQueries.searchByType;
+
+        search(
+            $("#SearchRequest").val(),
+            $("#IsStrictSearch").prop("checked"),
+            (teaList) => {
+                displayer.display(teaList);
+            });
+    }
 }
