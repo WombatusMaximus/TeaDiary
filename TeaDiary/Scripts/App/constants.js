da4a2349a3545c6d105fcf62f91cdf25e16f1b26
Wombatus <dovalkuvit@gmail.com>
2020-01-12 16:53:04 +0300

    Moved static class instances to singleton files out of constants

--- TeaDiary/Scripts/App/constants.js
+++ TeaDiary/Scripts/App/constants.js
@@ -1,17 +1,7 @@
-﻿var teaCommands;
-var apiCommands;
-var apiQueries;
-
-const FAILURE_TEXT = "Fail";
+﻿const FAILURE_TEXT = "Fail";
 const SUCCESS_TEXT = "Success";
 
 const SEARCH_TYPE = {
     NAME: "name",
     TYPE: "type"
 };
-
-$(document).ready(() => {
-    teaCommands = new TeaCommands();
-    apiCommands = new ApiCommands();
-    apiQueries = new ApiQueries();
-})
