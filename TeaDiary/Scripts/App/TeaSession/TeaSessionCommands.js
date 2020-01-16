f9609c3547778723c8d91919eb64d2a84aa1143e
Wombatus <dovalkuvit@gmail.com>
2020-01-16 22:36:52 +0300

    Moved TeaSession related scripts to their own folder

---
+++ TeaDiary/Scripts/App/TeaSession/TeaSessionCommands.js
@@ -0,0 +1,12 @@
+﻿function TeaSessionCommands() {
+    var self = this;
+    var confirmation = new PopupConfirmation();
+    self.delete = (id, successCallback, errorCallback) => {
+        confirmation.show(
+            {
+                body: "Are you sure you want to delete this tea session? You cannot undo this action"
+            },
+            () => apiCommands.deleteTeaSession(id, successCallback, errorCallback)
+        );
+    }
+}
