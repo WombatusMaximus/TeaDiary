1f4d6a7093ba7b7cfb9f191ab156a8556e8d3e7f
Wombatus <dovalkuvit@gmail.com>
2020-01-20 21:00:23 +0300

    Got rid of excessive constants

--- TeaDiary/Scripts/App/TeaSession/TeaSessionListDisplayer.constants.js
+++ TeaDiary/Scripts/App/TeaSession/TeaSessionListDisplayer.constants.js
@@ -1,16 +1,6 @@
 ﻿const TEA_SESSION_NUMBER = "teaSessionNumber";
 
 const TEA_SESSION_TABLE_HEAD = {
-    NUMBER: "#",
     DATE: "Date",
-    DETAILS: "Details",
-    MANAGE: "Actions",
     TEXT: "Description"
-}
-
-const TEA_SESSION_ELEMENT = {
-    PREFIX: "displayedTeaSessionElement",
-    ID: "Id",
-    CHECKBOX: "Checkbox",
-    LINK: "Link"
 }
